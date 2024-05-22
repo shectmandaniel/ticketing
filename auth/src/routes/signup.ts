43ca454fd85252d9cdb734154412ca86c5c9a6f4
Daniel Shechtman <shechtmandaniel@gmail.com>
2024-05-22 18:37:51 +0300

    Git ignore

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import 'express-async-errors'
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
@@ -14,7 +15,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
