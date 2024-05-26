eaa9c4478b8c5a2bc9135f82f57524a35c29950b
Daniel Shechtman <shechtmandaniel@gmail.com>
2024-05-26 10:14:50 +0300

    Added password hashing

--- auth/src/modals/user.ts
+++ auth/src/modals/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // An interface that describes the properties to create a new user
 interface UserAttr {
@@ -6,13 +7,13 @@ interface UserAttr {
   password: string;
 }
 
-interface UserModal extends mongoose.Model<any>{
-    build(attrs: UserAttr): UserDoc;
+interface UserModal extends mongoose.Model<any> {
+  build(attrs: UserAttr): UserDoc;
 }
 
 interface UserDoc extends mongoose.Document {
-    email: string;
-    password: string
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -26,11 +27,18 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttr) => {
   return new User(attrs);
 };
 
 const User = mongoose.model<UserDoc, UserModal>('User', userSchema);
-
 
 export { User };
