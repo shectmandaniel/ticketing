18a8acb6aaed38dfd725905b4d881dcb06fbd42f
Daniel Shechtman <shechtmandaniel@gmail.com>
2024-05-26 17:37:40 +0300

    Added test setup file

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,23 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+let mongo: any;
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  collections.forEach(async (collection) => await collection.deleteMany({}));
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
